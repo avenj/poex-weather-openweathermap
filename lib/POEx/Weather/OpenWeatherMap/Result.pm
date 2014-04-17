bfb59489a0f7413b91a9cdd2f6d918ed7dad5a37
Jon Portnoy <avenj@cobaltirc.org>
2014-04-17 18:23:17 -0400

    POD
    	modified:   lib/POEx/Weather/OpenWeatherMap/Result.pm

--- lib/POEx/Weather/OpenWeatherMap/Result.pm
+++ lib/POEx/Weather/OpenWeatherMap/Result.pm
@@ -80,3 +80,68 @@ has error => (
 );
 
 1;
+
+=pod
+
+=head1 NAME
+
+POEx::Weather::OpenWeatherMap::Result - Weather lookup result superclass
+
+=head1 SYNOPSIS
+
+  # Normally retrieved via POEx::Weather::OpenWeatherMap;
+  # subscribed sessions receive a subclass of this class:
+  sub my_weather_event {
+    my $result = $_[ARG0];
+    # ...
+  }
+
+=head1 DESCRIPTION
+
+This is the base class for L<POEx::Weather::OpenWeatherMap> weather results.
+
+Also see L<POEx::Weather::OpenWeatherMap::Result::Current> and
+L<POEx::Weather::OpenWeatherMap::Result::Forecast>.
+
+=head2 data
+
+This is the decoded hash from the attached L</json>. 
+
+Subclasses provide more convenient accessors for retrieving desired
+information.
+
+=head2 error
+
+The error message received from the OpenWeatherMap backend (or the empty
+string if there was no error).
+
+Also see L</is_success>, L</response_code>
+
+=head2 is_success
+
+Returns boolean true if the OpenWeatherMap backend returned a successful
+response.
+
+Also see L</error>, L</response_code>
+
+=head2 json
+
+The raw JSON this Result was created with.
+
+=head2 response_code
+
+The response code from OpenWeatherMap.
+
+Also see L</is_success>, L</error>
+
+=head2 request
+
+The original request that was attached to this result.
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
+
+Licensed under the same terms as perl.
+
+=cut
