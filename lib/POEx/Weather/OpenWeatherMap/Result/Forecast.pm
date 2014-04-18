29af39b67abdab57d22d6db520e2e284d7f98240
Jon Portnoy <avenj@cobaltirc.org>
2014-04-17 20:44:11 -0400

    Forecast & Forecast::Day classes
    	modified:   lib/POEx/Weather/OpenWeatherMap/Result/Forecast.pm
    	new file:   lib/POEx/Weather/OpenWeatherMap/Result/Forecast/Day.pm

---
+++ lib/POEx/Weather/OpenWeatherMap/Result/Forecast.pm
@@ -0,0 +1,63 @@
+package POEx::Weather::OpenWeatherMap::Result::Forecast;
+
+use v5.10;
+use strictures 1;
+use Carp;
+
+use Types::Standard      -all;
+use List::Objects::Types -all;
+
+use POEx::Weather::OpenWeatherMap::Result::Forecast::Day;
+
+
+sub lazy_for {
+  my $type = shift;
+  (
+    lazy => 1, is => 'ro', isa => $type,
+    ( $type->has_coercion ? (coerce => 1) : () ),
+    @_
+  )
+}
+
+use Moo; use MooX::late;
+extends 'POEx::Weather::OpenWeatherMap::Result';
+
+has id => ( lazy_for Int,
+  builder => sub { shift->data->{city}->{id} },
+);
+
+has name => ( lazy_for Str,
+  builder => sub { shift->data->{city}->{name} },
+);
+
+has country => ( lazy_for Str,
+  builder => sub { shift->data->{city}->{country} },
+);
+
+has latitude => ( lazy_for StrictNum,
+  builder => sub { shift->data->{city}->{coord}->{lat} },
+);
+
+has longitude => ( lazy_for StrictNum,
+  builder => sub { shift->data->{city}->{coord}->{lon} },
+);
+
+has count => ( lazy_for Int,
+  builder => sub { shift->data->{cnt} // 0 },
+);
+
+has _forecast_list => ( lazy_for ArrayObj,
+  builder => sub { 
+    my @list = @{ shift->data->{list} || [] };
+    [ map {;
+      ref %$_ eq 'HASH' ?
+        POEx::Weather::OpenWeatherMap::Result::Forecast::Day->new(%$_)
+        : carp "expected a HASH but got $_"
+    } @list ]
+  },
+);
+
+## FIXME Day objects for each item in _forecast_list
+
+
+1;
---
+++ lib/POEx/Weather/OpenWeatherMap/Result/Forecast/Day.pm
@@ -0,0 +1,166 @@
+package POEx::Weather::OpenWeatherMap::Result::Forecast::Day;
+
+use strictures 1;
+
+use Types::Standard       -all;
+use Types::DateTime       -all;
+use List::Objects::Types  -all;
+
+use POEx::Weather::OpenWeatherMap::Units -all;
+
+use Moo; use MooX::late;
+
+my $CoercedInt = Int->plus_coercions(StrictNum, sub { int });
+
+has dt => (
+  is        => 'ro',
+  isa       => DateTimeUTC,
+  coerce    => 1,
+  builder   => sub { 0 },
+);
+
+has pressure => (
+  is        => 'ro',
+  isa       => StrictNum,
+  builder   => sub { 0 },
+);
+
+has humidity => (
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { 0 },
+);
+
+has cloud_coverage => (
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { 0 },
+);
+
+
+has wind_speed_mph => (
+  init_arg  => 'speed',
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { 0 },
+);
+
+has wind_speed_kph => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { f_to_c shift->wind_speed_mph },
+);
+
+has wind_direction => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Str,
+  builder   => sub { deg_to_compass shift->wind_direction_degrees },
+);
+
+has wind_direction_degrees => (
+  init_arg  => 'deg',
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { 0 },
+);
+
+
+
+has temp => (
+  is        => 'ro',
+  isa       => InflatedHash,
+  coerce    => 1,
+  builder   => sub {
+    +{
+      morn => 0, night => 0, eve => 0, min => 0, max => 0, day => 0
+    }
+  },
+);
+
+has temp_min_f => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { shift->temp->min },
+);
+
+has temp_max_f => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { shift->temp->max },
+);
+
+has temp_min_c => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { f_to_c shift->temp_min_f },
+);
+
+has temp_min_f => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => $CoercedInt,
+  coerce    => 1,
+  builder   => sub { f_to_c shift->temp_max_f },
+);
+
+
+
+has _weather_list => (
+  init_arg  => 'weather',
+  is        => 'ro',
+  isa       => ArrayObj,
+  coerce    => 1,
+  builder   => sub { [] },
+);
+
+has _first_weather_item => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => HashRef,
+  builder   => sub { shift->_weather_list->[0] || +{} },
+);
+
+has conditions_terse => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Str,
+  builder   => sub { shift->_first_weather_item->{main} // '' },
+);
+
+has conditions_verbose => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Str,
+  builder   => sub { shift->_first_weather_item->{description} // '' },
+);
+
+has conditions_code => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Int,
+  builder   => sub { shift->_first_weather_item->{id} // 0 },
+);
+
+has conditions_icon => (
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Maybe[Str],
+  builder   => sub { shift->_first_weather_item->{icon} },
+);
+
+
+
+1;
