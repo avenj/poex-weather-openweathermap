6758b63d7e3c51302c35e59031036389b75c6600
Jon Portnoy <avenj@cobaltirc.org>
2014-04-18 22:01:12 -0400

    examples/; LWP example
    	new file:   examples/using_lwp.pl

---
+++ examples/using_lwp.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strictures 1;
+
+use LWP::UserAgent;
+
+use POEx::Weather::OpenWeatherMap::Request;
+use POEx::Weather::OpenWeatherMap::Result;
+
+my $location = shift(@ARGV) || 'Manchester, NH';
+
+my $request = POEx::Weather::OpenWeatherMap::Request->new_for(
+  Current => 
+    location => $location,
+);
+
+my $response = LWP::UserAgent->new->request( $request->http_request );
+die $response->status_line unless $response->is_success;
+
+my $result = POEx::Weather::OpenWeatherMap::Result->new_for(
+  Current =>
+    request => $request,
+    json    => $response->content,
+);
+
+my $place = $result->name;
+my $temp  = $result->temp_f;
+my $wind  = $result->wind_speed_mph;
+my $dir   = $result->wind_direction;
+
+print "$place -> ${temp}F, wind ${wind}mph ($dir)\n";
+
+
+
